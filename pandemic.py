d4e2f69f8accfbe1c68d18b523fe695e4e63d62d
Benjamin Grange <benjamin.grange@epitech.eu>
2017-09-12 17:29:39 +0200

    Added argument parsing & SIGINT catching.

--- pandemic.py
+++ pandemic.py
@@ -6,6 +6,8 @@
 
 import os
 import sys
+import argparse
+import signal
 
 from stat import *
 
@@ -27,15 +29,17 @@ def print_interesting_file(path, reason):
 def analyze_file(path):
     filestat = os.stat(path)
     filemode = filestat.st_mode
-    if filemode & S_ISUID != 0:
-        print_interesting_file(path, "SETUID")
 
-def walk_path(rootdir):
-    rootdir = os.path.abspath(rootdir)
+    " Look for ST_UID bit "
+    if filemode & S_ISUID != 0:
+        print_interesting_file(path, "ST_UID")
+
+def walk_path(rootdir, exclude_dirs):
     if not os.path.isdir(rootdir):
         print("[-] The given root path is not valid", file=sys.stderr)
         sys.exit(1)
     for root, subdirs, files in os.walk(rootdir):
+        subdirs[:] = [d for d in subdirs if os.path.join(root, d) not in exclude_dirs]
         for filename in files:
             try:
                 path = os.path.join(root, filename)
@@ -46,22 +50,34 @@ def walk_path(rootdir):
 def main():
     global found
 
-    if len(sys.argv) != 2:
-        print_usage()
-        sys.exit(1)
+    " Argument parsing "
+    parser = argparse.ArgumentParser(description = "Walks directory, looking for interesting files")
+    parser.add_argument("directory", metavar="dir", help="The directory to walk through")
+    parser.add_argument("-e", "--exclude", metavar="exclude_dir", default=["/dev", "/proc"], nargs="*", help="The directory to exclude (absolute path)")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Make output more verbose")
+    parser.add_argument("-V", "--version", action="store_true", help="Print version number")
+    args = parser.parse_args()
+
     print("[+] Pandemic {}.{}.{}".format(pandemic_major, pandemic_minor, pandemic_bug))
     print("[+] Made by the Rapace Diabolique")
     print("")
-    print("Searching from '{}'".format(sys.argv[1]))
+    print("Searching from '{}'".format(args.directory))
     print("")
 
-    walk_path(sys.argv[1])
+    signal.signal(signal.SIGINT, signal_handler)
+
+    rootdir = os.path.abspath(sys.argv[1])
+    walk_path(rootdir, args.exclude)
 
     if found:
         print("")
         print("[+] Analyze complete")
     else:
         print("[-] Nothing was found")
+
+def signal_handler(signal, frame):
+        print("[-] Search cancelled")
+        sys.exit(0)
 
 if __name__ == '__main__':
     main()
